d891629a1fc0dd43823bb3324c79d50e3517adc9
ayyadamufty-collab <ayyadamufty@gmail.com>
2026-09-14 16:17:06 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Pertanyaan Untuk Teman",
+    page_icon="💌"
+)
+
+st.title("💌 Pertanyaan Untuk Kamu")
+st.write("Jawab dengan jujur yaa 🤍")
+
+hal_suka = st.text_area("1. Hal-hal yang kamu suka")
+
+perlakuan = st.text_area("2. Kamu mau diperlakukan seperti apa?")
+
+kesan = st.text_area("3. Apa kesan pertama kamu tentang aku?")
+
+kesal = st.text_area("4. Kamu pernah kesal nggak sama sifat aku?")
+
+pesan = st.text_area("5. Kasih aku pesan")
+
+nama = st.text_input("6. Tulis nama kamu di sini ya")
+
+if st.button("Kirim Jawaban 💌"):
+    if hal_suka and perlakuan and kesan and kesal and pesan and nama:
+        st.success("Jawaban kamu sudah terkirim! 💕")
+
+        st.markdown("---")
+
+        st.title("❤️ I LOVE YOU ❤️")
+        st.write(f"Makasih sudah mau jawab, {nama}! 🫶")
+        st.write("Semoga kita tetap berteman baik yaa 🤍")
+    else:
+        st.warning("Jangan lupa isi semua pertanyaannya yaa 😄")
